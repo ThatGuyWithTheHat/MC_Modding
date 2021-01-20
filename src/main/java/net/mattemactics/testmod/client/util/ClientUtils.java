145a04ee827dedfb0266411a36686d6b0f42834b
Matt <MattDraft@Gmail.com>
2021-01-20 10:27:56 -0600

    Package setup complete per https://www.youtube.com/watch?v=-sG0H2lUV-U&list=PLaevjqy3Xufavi5mWXvWnGmwRylL-QZy7&index=3

---
+++ src/main/java/net/mattemactics/testmod/client/util/ClientUtils.java
@@ -0,0 +1,8 @@
+package net.mattemactics.testmod.client.util;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT) //this will make it crash if on server side
+public class ClientUtils {
+}
